a75b9c8f43bb2616b13d175b85bfbdc3b75afd00
ozkanmus <musa.ozkan@commencis.com>
2024-12-15 20:48:09 +0300

    Refactored code using socket and added concurrency

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import socket, threading
 
 # Check active ip addresses on your local machine by:
 # MacOs/Linux: ifconfig
@@ -6,14 +6,19 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 HOST = "127.0.0.1"
 PORT = 8080
 
-class HTTP(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Length", "0")
-        self.end_headers()
+def handle_client(client_socket, address):
+    print(f"New connection from {address}")
+    client_socket.sendall(b"Welcome to the server!\n")
+    client_socket.close()
 
 
-server = HTTPServer((HOST, PORT), HTTP)
-server.serve_forever()
-server.server_close()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
+
+print(f"Server listening on {HOST}:{PORT}")
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    thread.start()
